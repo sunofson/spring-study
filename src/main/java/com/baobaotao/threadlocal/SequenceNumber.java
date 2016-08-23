dece3d3092014c7b9bef2ca3a2f9488464421434
liyingjie <hzjsd1108@163.com>
2016-08-23 17:21:59 +0800

    #ThreadLocal demo

---
+++ src/main/java/com/baobaotao/threadlocal/SequenceNumber.java
@@ -0,0 +1,38 @@
+package com.baobaotao.threadlocal;
+
+public class SequenceNumber {
+	private ThreadLocal<Integer> seqNum = new ThreadLocal<Integer>() {
+		@Override
+		protected Integer initialValue() {
+			return 0;
+		}
+	};
+	
+	public int getNextNum() {
+		seqNum.set(seqNum.get() + 1);
+		return seqNum.get();
+	}
+	private static class TestClient extends Thread {
+		private SequenceNumber sn;
+		public TestClient(SequenceNumber sn) {
+			this.sn = sn;
+		}
+		@Override
+		public void run() {
+			for(int i = 0; i < 3; i ++) {
+				System.out.println("thread[" + Thread.currentThread().getName() + "]sn[" + sn.getNextNum()+"]");
+			}
+		}
+	}
+	
+	public static void main(String[] args) {
+		SequenceNumber sn = new SequenceNumber();
+		TestClient tc1 = new TestClient(sn);
+		TestClient tc2 = new TestClient(sn);
+		TestClient tc3 = new TestClient(sn);
+		
+		tc1.start();
+		tc2.start();
+		tc3.start();
+	}
+}
